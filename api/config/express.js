909a093202948dad1fbbcc3841aa024d222fb853
Bruno Almeida <bruno-ralmeida@outlook.com>
2021-03-06 11:20:30 -0300

    Ajuste na API

--- api/config/express.js
+++ api/config/express.js
@@ -2,7 +2,9 @@ var express = require('express')
     ,app = express()
     ,bodyParser = require('body-parser')
     ,routes = require('../app/routes');
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.static('./public'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
